65c9423d521f902be99419eb091d317c8deceb9f
Lawrence <39221871+Lawrence-ux@users.noreply.github.com>
2021-12-09 00:53:55 -0500

    Update libs.lua

--- libs.lua
+++ libs.lua
@@ -1,5 +1,40 @@
 --@Version[000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000003]
 
+--ffi
+ffi.cdef [[
+    void* GetProcAddress(void* hModule, const char* lpProcName);
+    void* GetModuleHandleA(const char* lpModuleName);
+    
+    typedef struct {
+        uint8_t r;
+        uint8_t g;
+        uint8_t b;
+        uint8_t a;
+    } color_struct_t;
+
+    typedef void (*console_color_print)(const color_struct_t&, const char*, ...);
+
+    typedef void* (__thiscall* get_client_entity_t)(void*, int);
+]]
+
+local ffi_log = ffi.cast("console_color_print", ffi.C.GetProcAddress(ffi.C.GetModuleHandleA("tier0.dll"), "?ConColorMsg@@YAXABVColor@@PBDZZ"))
+
+function client.log(msg, ...)
+    for k, v in pairs({...}) do
+        msg = tostring(msg .. v)
+    end
+    ffi_log(ffi.new("color_struct_t"), msg .. "\n")
+end
+
+function client.color_log(r, g, b, msg, ...)
+    for k, v in pairs({...}) do
+        msg = tostring(msg .. v)
+    end
+    local clr = ffi.new("color_struct_t")
+    clr.r, clr.g, clr.b, clr.a = r, g, b, 255
+    ffi_log(clr, msg .. "\n")
+end
+
 -- print table lib
 
 function print_table(node)
