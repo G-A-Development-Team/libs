5632cfc945b42714b31c34969ecad6e8bc48dec7
Lawrence <39221871+Lawrence-ux@users.noreply.github.com>
2021-12-09 20:01:43 -0500

    Update libs.lua

--- libs.lua
+++ libs.lua
@@ -1,4 +1,4 @@
---@Version[000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000007]
+--@Version[000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008]
 
 --ffi
 ffi.cdef [[
@@ -834,9 +834,7 @@ end
     
     
 -- gui custom stuff
-chicken = chicken or {}
-chicken.ui = chicken.ui or {}
-function chicken.ui.Create(ref, varname, name, x, y, w, h, paint, custom_vars)
+function gui._Custom(ref, varname, name, x, y, w, h, paint, custom_vars)
 	local tbl = {val = 0}
 
 	local function read(v)
@@ -862,44 +860,9 @@ function chicken.ui.Create(ref, varname, name, x, y, w, h, paint, custom_vars)
 		_element_width = w,
 		_element_height = h,
 		
-		-- For drawing children....
 		
-		
-		paint = paint,
-		_Children = {},
-		
-		_new_x = 0,
-		_new_y = 0,
-		_new_x2 = 0,
-		_new_y2 = 0,
-		
-		_child = false,
-		
-		addChild = function(self, child)
-			self._Children[#self._Children + 1] = child
-			self._Children[#self._Children]:SetChild()
-			
-			self._Children[#self._Children]._parent = self
-			
-			return self
-		
-		end,
-		
-		SetChild = function(self, parent)
-			self._child = true
-		end,
-		
-		IsChild = function(self)
-			return self._child
-		end,
-		
-		GetChildren = function(self)
-			return self._Children
-		end,
-		-- End children shit
 		
 		_parent = ref,
-		ParentIsChickenUI = false, 
 			
 		GetValue = function(self)
 			return self.element:GetValue()
@@ -971,51 +934,32 @@ function chicken.ui.Create(ref, varname, name, x, y, w, h, paint, custom_vars)
 			return self._parent
 		end,
 		
-		hovering = function(x, y, x2, y2)
-			local mx, my = input.GetMousePos()
-			return is_in_rect(mx, my, x, y, x2, y2)
-		end,
-						
-		_mouse_hovering = false,
-		_old_mouse_hovering = false,
-		
-		IsHovering = function(self)
-			return self._mouse_hovering
-		end,
-		
-		OnHovered = function(self) -- you rewrite this function when creating elements
-			
-		end,
-		
 		_mouse_left_released = true,
 		_old_mouse_left_released = true,
-		
-		OnMousePressed = function(self)
-			if self:IsHovering() then
-				if input.IsButtonDown(1) then -- left mouse button
-					return 1
-				elseif input.IsButtonDown(2) then-- right mouse button
-					return 2
-				end
-			end
-		end,
 		
 		OnClick = function(self) -- you rewrite this function when creating elements
 			
 		end,
+				
+		hovering = function(x, y, x2, y2)
+			local mx, my = input.GetMousePos()
+			return is_in_rect(mx, my, x, y, x2, y2)
+		end,
+		
+		_mouse_hovering = false,
+		_old_mouse_hovering = false,
+		OnHovered = function(self)
+			
+		end,	
 	}
-	
-	
-	
 	
 	local meta = {__index = custom_vars}
 	setmetatable(GuiObject, meta)
 	
 	local function _paint(x, y, x2, y2, active)
-		-- hovering / clicking events
+	
 		local mx, my = input.GetMousePos()
-		
-		local hovering = GuiObject.hovering(x, y, x2, y2)	
+		local hovering = GuiObject.hovering(x, y, x2, y2)
 		
 		if hovering then
 			GuiObject._mouse_hovering = true		
@@ -1038,28 +982,14 @@ function chicken.ui.Create(ref, varname, name, x, y, w, h, paint, custom_vars)
 		end
 
 		if GuiObject._old_mouse_hovering ~= GuiObject._mouse_hovering then
+			-- print(GuiObject._mouse_hovering)
 			GuiObject:OnHovered(GuiObject._mouse_hovering)
 			GuiObject._old_mouse_hovering = GuiObject._mouse_hovering
 		end
 		
 		local width = x2 - x
 		local height = y2 - y
-		
-		if not GuiObject:IsChild() then
-			paint(x, y, x2, y2, active, GuiObject, width, height)
-		end
-		
-		--- Draw children objects
-		for i, v in pairs(GuiObject:GetChildren()) do
-			-- print(i)
-			local new_x = x + v._element_pos_x
-			local new_y = y2 - GuiObject._element_height + v._element_pos_y  
-			
-			local new_x2 = x2 - GuiObject._element_width + v._element_width + v._element_pos_x
-			local new_y2 = y + v._element_pos_y  + v._element_height
-			
-			v.paint(new_x, new_y, new_x2, new_y2, _, v, v._element_width, v._element_height)		
-		end
+		paint(x, y, x2, y2, active, GuiObject, width, height)
 	end
 	
 	local custom = gui.Custom(ref, varname, x, y, w, h, _paint, write, read)
@@ -1069,44 +999,15 @@ function chicken.ui.Create(ref, varname, name, x, y, w, h, paint, custom_vars)
 end
 
 
-function chicken.ui.ColoredText(ref, text, x, y, options)
+function gui.ColoredText(ref, text, x, y, options)
 	local function paint(x, y, x2, y2, active, self, width, height)
-		-- print(1)
 		local options = self.custom_vars
 	
 		-- text
 		draw.Color(options.text_color[1], options.text_color[2], options.text_color[3])		
 		draw.SetFont(options.font)
+		draw.Text(x, y, options.text)		
 		
-		-- parse \n 
-		-- todo text wrap
-		if not self.custom_vars.fixed_text then
-			local new_index = 1
-			local len = string.len(options.text)
-			for i=1, len do
-				local current_char = string.sub(options.text, i, i)
-				if current_char == "\n" then
-					self.custom_vars.lines[#self.custom_vars.lines + 1] = string.sub(options.text, new_index, i - 1)
-					new_index = i + 1
-				end
-			end
-			if not string.find(options.text, "\n") then self.custom_vars.lines[#self.custom_vars.lines + 1] = options.text end
-			self.custom_vars.fixed_text = true
-		end
-		
-		
-		
-		for i, line in ipairs(options.lines) do
-			local spacer = 3
-			-- print(line)
-			local text_x, text_y = draw.GetTextSize(line)
-			if #options.lines > 1 then
-				draw.Text(x, y + i * text_y + i * spacer, line)
-			else
-				draw.Text(x, y, line)
-			end
-		end
-				
 		--underline
 		if options.underline then
 			local text_x, text_y = draw.GetTextSize(options.text)
@@ -1123,18 +1024,15 @@ function chicken.ui.ColoredText(ref, text, x, y, options)
 		text = text,
 		text_color = options.text_color and {options.text_color[1] or 255, options.text_color[2] or 255, options.text_color[3] or 255, options.text_color[4] or 255} or {255,255,255,255},
 		font = options.font or draw.CreateFont("Bahnschrift", 14),
-		underline = options.underline or false,
 		
-		lines = {},
-		fixed_text = false
-
+		underline = options.underline or false,
 	}
 	vars.underline_color = options.underline_color and {options.underline_color[1] or 255, options.underline_color[2] or 255, options.underline_color[3] or 255, options.underline_color[4] or 255} or vars.text_color
 
 	
 
 	local text_x, text_y = draw.GetTextSize(text)
-	local custom = chicken.ui.Create(ref, "", "", x, y, text_x, text_y, paint, vars)
+	local custom = gui._Custom(ref, "", "", x, y, text_x, text_y, paint, vars)
 		
 	function custom:SetOptions(options)
 		vars.text = options.text or vars.text
@@ -1150,7 +1048,7 @@ function chicken.ui.ColoredText(ref, text, x, y, options)
 	return custom
 end
 
-function chicken.ui.LinkText(ref, text, x, y, options)
+function gui.LinkText(ref, text, x, y, options)
 
 	local linked_text = gui.ColoredText(ref, text, x, y, {text_color = {0, 70, 255}})	
 	linked_text.OnHovered = function(self, IsHovering)
@@ -1164,72 +1062,20 @@ function chicken.ui.LinkText(ref, text, x, y, options)
 	return linked_text
 end
 
-function chicken.ui.Image(ref, img_texture_params, x, y, w, h)
-	local function paint(x, y, x2, y2, active, self, width, height)
-		
-		if not self.custom_vars.texture then return end
-		draw.Color(255,0,0, 255)
-		
-		if self:IsHovering() then
-			draw.Color(255,255,255, 200)
-		end
-		
-		
-		if self:OnMousePressed() == 1 then
-			y = y + height / 20
-			x = x + width / 20
-			
-			x2 = x2 - height / 20
-			y2 = y2 - width / 20
-		end
-		
-		draw.SetTexture(self.custom_vars.texture)
-		
-		draw.Color(255,255,255,255)
-		draw.FilledRect(x,y,x2,y2)
-		
-	end
 
-	local texture = draw.CreateTexture(img_texture_params[1], img_texture_params[2], img_texture_params[3])
-	
-	local vars = {
-		texture = texture,
-		paint = paint
-	}
-	
-	local custom = 	chicken.ui.Create(ref, "Images", "test2", x, y, w, h, paint, vars)
-	
-	function custom:SetTexture(img_texture_params)
-		vars.texture = draw.CreateTexture(img_texture_params[1], img_texture_params[2], img_texture_params[3])
-	end
-	
-	return custom
-end
-
-local test_tab = gui.Tab(gui.Reference("Settings"), "test.tab", "Test tab")
-
-function chicken.ui.GroupBox(ref, x, y, w, h)
-	local function paint(x, y, x2, y2, active, self, width, height)
-		draw.Color(0,0,0, 100)
-
-		draw.FilledRect(x, y, x2, y2)
-		draw.Color(unpack(self.custom_vars.header_bg))
-		draw.FilledRect(x, y, x2, y2 - (h / 1.25))
-		
-		local children_height = 0		
-	end
-	
-	local font = draw.CreateFont("Arial", 18)
+-- Examples
 
 
-	local vars = {
-		header_bg = {150,0,0}
-	}
-	
-	local custom = 	chicken.ui.Create(ref, "", "", x, y, w, h, paint, vars)
-	custom.header_text = chicken.ui.ColoredText(test_tab, "Lua loader V2 | Made by chicken",5,5, {font = font})
-	custom:addChild(custom.header_text)
-	
-	
-	return custom
-end
+-- local test_tab = gui.Tab(gui.Reference("Misc"), "test.tab", "Test tab")
+
+-- local font = draw.CreateFont("Bahnschrift", 14)
+-- local text = gui.ColoredText(test_tab, "Hello world", 100, 100, {
+	-- font = font,
+	-- text_color = {255,0,0}
+-- })
+
+-- text.OnClick = function(self)
+	-- self:SetOptions({text = "I have been clicked!"})
+-- end
+
+-- local linked_texted = gui.LinkText(test_tab, "Hello world", 200, 200)
