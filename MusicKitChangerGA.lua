dd87e64209a2637d52b0d349da8bd2dfb1393016
Lawrence <39221871+Lawrence-ux@users.noreply.github.com>
2021-12-13 17:54:09 -0500

    Create MusicKitChangerGA.lua

---
+++ MusicKitChangerGA.lua
@@ -0,0 +1,42 @@
+--------------------------------------------
+--          READ JSON EXECUTION           --
+-- Credit To: Chicken4676                 --
+-- Credit To: tg021 (Github)              --
+--------------------------------------------
+
+local json_lib_installed = false
+file.Enumerate(function(filename)
+    if filename == "libraries/json.lua" then
+        json_lib_installed = true
+    end
+end)
+
+if not json_lib_installed then
+    local body = http.Get("https://raw.githubusercontent.com/G-A-Development-Team/libs/main/json.lua")
+    file.Write("libraries/json.lua", body)
+end
+
+RunScript("libraries/json.lua")
+
+
+--------------------------------------------
+--          READ JSON EXECUTION           --
+--------------------------------------------
+
+local MusicKitChanger = {}
+MusicKitChanger.Json = http.Get('https://raw.githubusercontent.com/G-A-Development-Team/libs/main/music.json')
+print(json.decode(MusicKitChanger.Json))
+MusicKitChanger.List = json.decode(MusicKitChanger.Json)['kits']['kits']
+MusicKitChanger.Gui = gui.Combobox(gui.Reference("Visuals", "Other", "Extra"), "MKC_KitName", "Music Kit Changer", unpack(MusicKitChanger.List))
+MusicKitChanger.Gui:SetDescription("Changes your music kit.")
+MusicKitChanger.Fire = function()
+    if entities.GetLocalPlayer() == nil then return end
+    local kit = MusicKitChanger.Gui:GetValue()
+    if kit == 0 then return end
+    entities.GetPlayerResources():SetPropInt(kit, "m_nMusicID", client.GetLocalPlayerIndex())
+end
+
+
+callbacks.Register("Draw", function()
+    MusicKitChanger.Fire()
+end)
