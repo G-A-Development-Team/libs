ac35f6b9936d4e40a2fafd62c5f2d9ef232fad56
Carter Poe <39221871+Carter-Poe@users.noreply.github.com>
2022-07-24 18:51:00 -0400

    add

--- SwitchTeamAPI.lua
+++ SwitchTeamAPI.lua
@@ -1,16 +1,18 @@
 SwitchTeamCallback = nil
 
 callbacks.Register("FireGameEvent", function(event)
-	if event:GetName() == "round_end" then SwitchTeamCallback() end
-	if event:GetName() == "round_start" then SwitchTeamCallback() end
-	if event:GetName() == "round_poststart" then SwitchTeamCallback() end
-	if event:GetName() == "round_prestart" then SwitchTeamCallback() end
-	if event:GetName() == "teamplay_round_start" then SwitchTeamCallback() end
-	if event:GetName() == "round_officially_ended" then SwitchTeamCallback() end
-	if event:GetName() == "teamchange_pending" then SwitchTeamCallback() end
-	if event:GetName() == "jointeam_failed" then SwitchTeamCallback() end
-	if event:GetName() == "player_death" then SwitchTeamCallback() end
-	if event:GetName() == "cs_win_panel_round" then SwitchTeamCallback() end
+    if SwitchTeamCallback ~= nil then
+        if event:GetName() == "round_end" then SwitchTeamCallback() end
+        if event:GetName() == "round_start" then SwitchTeamCallback() end
+        if event:GetName() == "round_poststart" then SwitchTeamCallback() end
+        if event:GetName() == "round_prestart" then SwitchTeamCallback() end
+        if event:GetName() == "teamplay_round_start" then SwitchTeamCallback() end
+        if event:GetName() == "round_officially_ended" then SwitchTeamCallback() end
+        if event:GetName() == "teamchange_pending" then SwitchTeamCallback() end
+        if event:GetName() == "jointeam_failed" then SwitchTeamCallback() end
+        if event:GetName() == "player_death" then SwitchTeamCallback() end
+        if event:GetName() == "cs_win_panel_round" then SwitchTeamCallback() end
+    end
 end)
 
 client.AllowListener("cs_win_panel_round")
