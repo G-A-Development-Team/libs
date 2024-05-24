1f9402995e62633b3e6256663119dcd144b2c6db
Jeremy <spamalotsmileface@gmail.com>
2024-05-23 22:53:51 -0700

    Update library_loader.lua

--- library_loader.lua
+++ library_loader.lua
@@ -1,24 +1,26 @@
 Libraries = {
-    "json" = "https://raw.githubusercontent.com/G-A-Development-Team/libs/main/json.lua",
-    "string" = "https://raw.githubusercontent.com/G-A-Development-Team/libs/main/string.lua",
-    "table" = "https://raw.githubusercontent.com/G-A-Development-Team/libs/main/table.lua"
+    "json"   = { found = false, url = "https://raw.githubusercontent.com/G-A-Development-Team/libs/main/json.lua" },
+    "string" = { found = false, url = "https://raw.githubusercontent.com/G-A-Development-Team/libs/main/string.lua" },
+    "table"  = { found = false, url = "https://raw.githubusercontent.com/G-A-Development-Team/libs/main/table.lua" }
 }
 
-local created = {}
-
 file.Enumerate( function( filename )
-    for loc, url in pairs( Libraries ) do
+    
+    for loc, data in pairs( Libraries ) do
         if filename == "libraries/" .. loc .. ".lua" then
-            loaded[ loc ] = url
+            Libraries[ loc ].found = true
         end
     end
+
 end)
 
-for loc, url in pairs( loaded ) do
-    local body = http.Get( url )
-    file.Write("libraries/" .. loc .. ".lua", body)
+for loc, data in pairs( loaded ) do
+    if not Libraries[ loc ].found then
+        local body = http.Get( data.url )
+        file.Write("libraries/" .. loc .. ".lua", body)
+    end
 end
 
-for loc, url in pairs( Libraries ) do
+for loc, data in pairs( Libraries ) do
     RunScript("libraries/" .. loc .. ".lua")
 end
