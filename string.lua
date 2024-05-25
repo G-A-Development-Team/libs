4e91c777a09f78daaad9e45872dd9d8a2ae6391a
Jeremy <spamalotsmileface@gmail.com>
2024-05-25 13:37:57 -0700

    Update string.lua
    Removed ToTable
    Removed Explode
    
    
    Library Currently Broken

--- string.lua
+++ string.lua
@@ -1,33 +1,3 @@
-function string.ToTable( input )
-	local tbl = {}
-
-	local str = tostring( input )
-
-	for i = 1, #str do
-		tbl[i] = string.sub( str, i, i )
-	end
-
-	return tbl
-end
-
-function string.Explode( separator, str, withpattern )
-	if ( separator == "" ) then return string.ToTable( str ) end
-	if ( withpattern == nil ) then withpattern = false end
-
-	local ret = {}
-	local current_pos = 1
-
-	for i = 1, string.len( str ) do
-		local start_pos, end_pos = string.find( str, separator, current_pos, not withpattern )
-		if ( not start_pos ) then break end
-		ret[ i ] = string.sub( str, current_pos, start_pos - 1 )
-		current_pos = end_pos + 1
-	end
-
-	ret[ #ret + 1 ] = string.sub( str, current_pos )
-
-	return ret
-end
 
 function string.Split( str, delimiter )
 	return string.Explode( delimiter, str )
