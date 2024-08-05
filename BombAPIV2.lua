69a8a12b739ff22255e108d646fbac08396f7639
Carter <gamechampcrafted@gmail.com>
2024-08-05 17:24:08 -0400

    Update BombAPIV2.lua

--- BombAPIV2.lua
+++ BombAPIV2.lua
@@ -1,21 +1,17 @@
-class_plantedC = "C_PlantedC4"
-
 timePlanted = 0 
 defusing = false 
 ended = false
 
 function UpdateBombInfo()
     local pC4               = (entities.FindByClass("C_C4"))[1];
-    local pPlantedC4        = (entities.FindByClass(class_plantedC))[1];
+    local pPlantedC4        = (entities.FindByClass("C_PlantedC4"))[1];
     if not pC4 and not pPlantedC4 then
-        --g_iBombState = BOMB_NOTFOUND;
         return;
 
     elseif pPlantedC4 then
 
         if not pPlantedC4:GetPropBool("m_bBombTicking") then
-            --g_iBombState = BOMB_DEAD;
-        
+
         elseif pPlantedC4:GetPropBool("m_bBeingDefused") then
             defusing = true ended = false
         else
@@ -42,19 +38,19 @@ function BombDamage(Bomb, Player)
 	local Gauss = (C4Distance - 75.68) / 789.2 
 	local flDamage = 450.7 * math.exp(-Gauss * Gauss);
 
-		if Player:GetProp("m_ArmorValue") > 0 then
+		if Player:GetPropInt("m_ArmorValue") > 0 then
 
 			local flArmorRatio = 0.5;
 			local flArmorBonus = 0.5;
 
-			if Player:GetProp("m_ArmorValue") > 0 then
+			if Player:GetPropInt("m_ArmorValue") > 0 then
 			
 				local flNew = flDamage * flArmorRatio;
 				local flArmor = (flDamage - flNew) * flArmorBonus;
 			 
-				if flArmor > Player:GetProp("m_ArmorValue") then
+				if flArmor > Player:GetPropInt("m_ArmorValue") then
 				
-					flArmor = Player:GetProp("m_ArmorValue") * (1 / flArmorBonus);
+					flArmor = Player:GetPropInt("m_ArmorValue") * (1 / flArmorBonus);
 					flNew = flDamage - flArmor;
 					
 				end
@@ -69,9 +65,11 @@ function BombDamage(Bomb, Player)
 	
 end
 
+--[[
 client.AllowListener("bomb_planted")
 client.AllowListener("bomb_begindefuse")
 client.AllowListener("bomb_abortdefuse") 
 client.AllowListener("bomb_exploded")
 client.AllowListener("round_officially_ended")
 client.AllowListener("bomb_defused") 
+--]]
