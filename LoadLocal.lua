f473866ef214d8742b380edebca28880d45a4c97
Carter <gamechampcrafted@gmail.com>
2024-08-13 21:11:14 -0400

    Update LoadLocal.lua

--- LoadLocal.lua
+++ LoadLocal.lua
@@ -1,9 +1,11 @@
 function LoadLocal(sfile)
     sfile = sfile .. ".lua"
+    local suffix_length = #sfile
+
     file.Enumerate(function(fil)
         -- Check if 'fil' ends with the suffix specified by 'sfile'
-        if string.match(fil, sfile .. "$") then
-            LoadScript(fil)
+        if #fil >= suffix_length and string.sub(fil, -suffix_length) == sfile then
+            LoadScript( fil )
         end
     end)
 end
